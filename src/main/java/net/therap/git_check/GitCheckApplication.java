1871f5125e511f649242bd55f52076346c38be05
Md. Iftakher Ahmad <iftakhar.ahmed@therapservices.net>
2022-01-25 15:26:33 +0600

    Fixed merge conflict

---
+++ src/main/java/net/therap/git_check/GitCheckApplication.java
@@ -0,0 +1,12 @@
+package net.therap.git_check;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitCheckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitCheckApplication.class, args);
+    }
+}
---
+++ src/test/java/net/therap/git_check/GitCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package net.therap.git_check;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitCheckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
